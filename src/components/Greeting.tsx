849736723b6fd06ae234038821f8c3322b6afb75
Ben Polinsky <benjamin.polinsky@gmail.com>
2023-11-10 16:05:38 -0500

    finish tutorial

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,14 @@
+import {useState} from "preact/hooks";
+
+export default function Greeting({messages}: {messages: string[]}){
+    const randomNumber = () => (Math.floor(Math.random() * messages.length))
+    const randomMessage = () => messages[randomNumber()]
+    const [greeting, setGreeting] = useState(messages[0])
+
+    return (
+        <div>
+            <h3>{greeting}! Thanks for visiting</h3>
+            <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+        </div>
+    )
+}
---
+++ src/pages/blog/post-4.md
@@ -0,0 +1,13 @@
+---
+layout: ../../layouts/BlogLayout.astro
+title: My Fourth Blog Post
+author: Thug B
+description: "I had some challenges, but asking in the community really helped!"
+image:
+    url: "https://docs.astro.build/assets/rays.webp"
+    alt: "Thumbnail of Astro rays."
+pubDate: 2022-07-15
+tags: ["astro", "learning in public", "setbacks", "community", "thug b"]
+---
+
+Really about time for a new Thug B album. Perhaps Camouflage Chameleon is Thug B
